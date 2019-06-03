db0bfa7a801472470496f81a35c4dac1c1f86d38
michal.lipski <michal.lipski@newvoicemedia.com>
2019-06-03 14:50:41 +0200

    first commit

---
+++ src/main/java/tdd/bonus/BonusCalculator.java
@@ -0,0 +1,5 @@
+package tdd.bonus;
+
+public class BonusCalculator {
+
+}
---
+++ src/test/java/tdd/bonus/BonusCalculatorTest.java
@@ -0,0 +1,13 @@
+package tdd.bonus;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BonusCalculatorTest {
+
+    @Test
+    public void startHere() {
+        assertThat(4).isEqualTo(5);
+    }
+}
