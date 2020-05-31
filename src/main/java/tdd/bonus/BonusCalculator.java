25749fa9ce26a87604361e2e41754a3c73d038be
michal.lipski <kiwaczki@gmail.com>
2020-05-31 14:20:27 +0200

    solution for first part

--- src/main/java/tdd/bonus/BonusCalculator.java
+++ src/main/java/tdd/bonus/BonusCalculator.java
@@ -2,4 +2,21 @@ package tdd.bonus;
 
 public class BonusCalculator {
 
+    public Integer calculate(int sales, int quota, int commission, int tax) {
+        int bonus;
+        if (sales <= quota) {
+            return 0;
+        }
+        bonus = sales - quota;
+        bonus = withCommission(commission, bonus);
+        return taxed(tax, bonus);
+    }
+
+    private int taxed(int tax, int value) {
+        return value - value * tax / 100;
+    }
+
+    private int withCommission(int commission, int value) {
+        return value * commission / 100;
+    }
 }
--- src/test/java/tdd/bonus/BonusCalculatorTest.java
+++ src/test/java/tdd/bonus/BonusCalculatorTest.java
@@ -7,7 +7,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BonusCalculatorTest {
 
     @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    public void no_bonus_when_sales_less_than_quota() {
+        Integer bonus = new BonusCalculator().calculate(90, 100, 10, 0);
+
+        assertThat(bonus).isEqualTo(0);
     }
+
+    @Test
+    public void no_bonus_when_sales_equal_quota() {
+        Integer bonus = new BonusCalculator().calculate(100, 100, 100, 0);
+
+        assertThat(bonus).isEqualTo(0);
+    }
+
+    @Test
+    public void bonus_is_given_when_sales_grater_that_quota() {
+        Integer bonus = new BonusCalculator().calculate(1000, 900, 100, 0);
+
+        assertThat(bonus).isEqualTo(100);
+    }
+
+    @Test
+    public void bonus_value_depend_on_commission() {
+        Integer bonus = new BonusCalculator().calculate(1000, 900, 10, 0);
+
+        assertThat(bonus).isEqualTo(10);
+    }
+
+    @Test
+    public void bonus_value_depend_on_tax() {
+        Integer bonus = new BonusCalculator().calculate(1000, 900, 10, 10);
+
+        assertThat(bonus).isEqualTo(9);
+    }
+
+
 }
